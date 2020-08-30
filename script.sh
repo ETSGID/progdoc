5724f0341e480013c15048a37414968405d70680
javicond3 <4b3javi@gmail.com>
2020-08-30 20:28:58 +0200

    update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 echo "Hi, I'm sleeping until the database is up..."
 sleep 12
-echo "Hi, I´m sleeping until the migrations are executed..."
+echo "Executing migrations..."
 npm run migrations
-sleep 10
-echo "Hi, I´m sleeping until the seeders are executed..."
+echo "Executing seeders..."
 npm run seeders
-sleep 3
 npm start 
 echo "all Done."
 exit 0
