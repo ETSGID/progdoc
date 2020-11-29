659c01ba1f7a40e6e448ca10c86b8ce62b8c7b40
JAVIER <javier.conde.diaz@alumnos.upm.es>
2020-11-29 13:21:14 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,8 +3,8 @@ echo "Hi, I'm sleeping until the database is up..."
 sleep 12
 echo "Executing migrations..."
 npm run migrations
-echo "Executing seeders..."
-npm run seeders
+#echo "Executing seeders..."
+#npm run seeders
 npm start 
 echo "all Done."
 exit 0
